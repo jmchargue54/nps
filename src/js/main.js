208b66d63012c06f5425cadabb994be87c9f5241
jmchargue54 <jmchargue54@gmail.com>
2024-11-06 09:38:23 -0700

    nps1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+// disclaimer url 
+const disclaimer = document.querySelector(".disclaimer_url");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+// title
+const titleEl = document.querySelector("title");
+titleEl.innerHTML = parkData.fullName;
+
+// banner 
+function parkInfoTemplate(info) {
+    return `<a href="#" class="banner-title">Yellowstone</a>
+          <p class="banner-subtitle">
+            <span>${info.designation}</span>
+            <span>${info.states}</span>
+          </p>`;
+}
+function renderParkInfo() {
+    const bannerEl = document.querySelector("#banner");
+    bannerEl.innerHTML = parkInfoTemplate(parkData)
+}
+// image
+const imgEl = document.querySelector("#banner-img");
+imgEl.src = parkData.images[0].url;
+
+
+renderParkInfo();
